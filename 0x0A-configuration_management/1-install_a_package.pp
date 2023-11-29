baed6a8e127b61f1dc636602e75c9d4d2f290954
yego5 <shachz.github@gmail.com>
2023-11-29 14:44:36 -0800

    install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# Install flask version 2.1.0 package from pip3
-
+# Install an especific version of flask (2.1.0)
 package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }
